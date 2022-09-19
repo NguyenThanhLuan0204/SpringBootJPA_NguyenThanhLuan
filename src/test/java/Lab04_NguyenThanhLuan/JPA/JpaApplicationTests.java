aac956c1efc812a17b7c19cc8265d936985feda2
Nguyễn Thành Luân <75401588+takarahunter@users.noreply.github.com>
2022-09-19 10:20:31 +0700

    Initial commit

---
+++ src/test/java/Lab04_NguyenThanhLuan/JPA/JpaApplicationTests.java
@@ -0,0 +1,13 @@
+package Lab04_NguyenThanhLuan.JPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
